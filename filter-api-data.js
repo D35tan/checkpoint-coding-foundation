f18917a2d7c64ea3fae8b99aee85a69095ddbf84
Tolga Durdagi <durdagi.tolga@gmail.com>
2022-07-28 20:16:45 +0200

    feat: adds checkpoint int.state

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,19 @@
-function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
-}
+/*
+
+const objID = apiData.filter(function (data) {
+  return data.id >= 0 && data.price >= 0;
+});
+const mandatoryKeys = objID.map((data) => {
+  return;
+  {
+    id: data.id;
+    price: data.price;
+  }
+});
+
+/*Sackgasse*/
+
+console.log(mandatoryKeys);
+*/
+
+function filterApiData(apiData, mandatoryKeys) {}
--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,29 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  // Strings splitten
+  //strA = 1234
+  const strAArr = strA.split("");
+  //strB = abc
+  const strBArr = strB.split("");
+  //Variable für Ergebnis
+  // Soll "1a2b3c4" ergeben
+  let zippedStr = "";
+  // variable zur Bestimmung des längeren Strings
+  let longestStr;
+  // if Abfrage zur Bestimmung des längeren Strings
+  if (strAArr.length > strBArr.length) {
+    longestStr = strAArr.length;
+  } else {
+    longestStr = strBArr.length;
+  }
+  //Zip-Logik
+  for (let i = 0; i < longestStr; i++) {
+    if (strAArr[i] === undefined) {
+      zippedStr += strBArr[i];
+    } else if (strBArr[i] === undefined) {
+      zippedStr += strAArr[i];
+    } else {
+      zippedStr = strAArr[i] + strBArr[i];
+    }
+  }
+  return zippedStr;
 }
