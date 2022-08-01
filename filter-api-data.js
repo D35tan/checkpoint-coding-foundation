dad6ac7ad8200cd29716d123a5c1c3a8e98b7fdb
Tolga Durdagi <durdagi.tolga@gmail.com>
2022-08-01 09:54:40 +0200

    feat: new solution approach

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,19 +1,6 @@
-/*
-
-const objID = apiData.filter(function (data) {
-  return data.id >= 0 && data.price >= 0;
-});
-const mandatoryKeys = objID.map((data) => {
-  return;
-  {
-    id: data.id;
-    price: data.price;
-  }
-});
-
-/*Sackgasse*/
-
-console.log(mandatoryKeys);
-*/
-
-function filterApiData(apiData, mandatoryKeys) {}
+function filterApiData(apiData, mandatoryKeys) {
+  const results = apiData.filter(function (keyFilter) {
+    return Object.keys(keyFilter).includes(...mandatoryKeys);
+  });
+  return results;
+}
